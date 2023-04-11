7ee884ccaae0175a61533aaa3af418e3bdb487ed
KostasMateer <komateer@davidson.edu>
2023-04-11 01:45:48 -0400

    prototype done

--- index.py
+++ index.py
@@ -1,39 +1,29 @@
-from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, render_template, jsonify
 from google.cloud import language_v1
-from google.oauth2.service_account import Credentials
+from textblob import TextBlob
 from dotenv import load_dotenv
 load_dotenv()
 
 # Instantiates a client
 client = language_v1.LanguageServiceClient()
-
-# import joblib
-# import os
-
-# # Load the trained machine learning model
-# model = joblib.load('trained_model.pkl')
-
 app = Flask(__name__, static_folder='static')
-CORS(app)
 
 @app.route('/')
 def index():
-    # if request.method == 'POST':
-    #     text_input = request.form['text_input']
-    #     prediction = (text_input)
-    #     return render_template('index.html', prediction=prediction, text_input=text_input)
     return render_template('index.html')
 
 # Define a route to accept POST requests
 @app.route('/predict/<text>', methods=['GET'])
-@cross_origin()
 def predict(text):
-    # Get data from the POST request
     textToSentiment = text
     document = language_v1.Document(content=textToSentiment, type_=language_v1.Document.Type.PLAIN_TEXT)
-    sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment.score
-    return jsonify({'sentiment': sentiment})
+    google_sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment.score
+    python_sentiment = TextBlob(textToSentiment).sentiment.polarity
+    return jsonify(
+                    {'google_sentiment': google_sentiment,
+                    "python_sentiment": python_sentiment}
+                    )
+
 
 @app.route('/api')
 def server_live():
--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,21 @@
-$('#python-form').submit(function(event) {
+$('#google-form').submit(function(event) {
   event.preventDefault(); // prevent default form submit behavior
   var inputVal = $('#input-field').val();
-  fetch("http://localhost:5000/predict/" + inputVal)
+  if (inputVal) {
+    fetch("https://dig345-p2-emotionai.vercel.app/predict/" + inputVal)
+    // fetch("http://localhost:5000/predict/" + inputVal)
     .then((response) => {
       // convert to json
       return response.json();
     })
     .then((data) => {
-      console.log(data.prediction)
-      $("#python-prediction").html(`Prediction: ${data.sentiment}`)
+      $("#google-prediction").html(`Prediction: ${data.google_sentiment}`)
+      $("#python-prediction").html(`Prediction: ${data.python_sentiment}`)
     })
     .catch((err) => console.log(err));
+  }
+  else {
+    $("#google-prediction").html(`Prediction: no text`)
+    $("#python-prediction").html(`Prediction: no text`)
+  }
 })
