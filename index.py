23bf7309889fdfc987b3926c2430aa214f81a91f
KostasMateer <komateer@davidson.edu>
2023-04-27 13:36:55 -0400

    final update to git hub repo

--- index.py
+++ index.py
@@ -1,33 +1,52 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from google.cloud import language_v1
 from textblob import TextBlob
 from dotenv import load_dotenv
+from flask_cors import CORS
+
 load_dotenv()
 
 # Instantiates a client
+app = Flask(__name__, static_folder="static")
+CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 client = language_v1.LanguageServiceClient()
-app = Flask(__name__, static_folder='static')
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
 
 # Define a route to accept POST requests
-@app.route('/predict/<text>', methods=['GET'])
-def predict(text):
+@app.route("/predict", methods=["POST"])
+def predict():
+    text = request.json.get("text", "")
     textToSentiment = text
-    document = language_v1.Document(content=textToSentiment, type_=language_v1.Document.Type.PLAIN_TEXT)
-    google_sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment.score
-    python_sentiment = TextBlob(textToSentiment).sentiment.polarity
+    document = language_v1.Document(
+        content=textToSentiment, type_=language_v1.Document.Type.PLAIN_TEXT
+    )
+
+    google_sentiment = round(client.analyze_sentiment(
+        request={"document": document}
+    ).document_sentiment.score, 2)
+
+    python_sentiment = round(TextBlob(textToSentiment).sentiment.polarity, 2)
+
     return jsonify(
-                    {'google_sentiment': google_sentiment,
-                    "python_sentiment": python_sentiment}
-                    )
+        {"google_sentiment": google_sentiment, "python_sentiment": python_sentiment}
+    )
 
 
-@app.route('/api')
+@app.route("/api")
 def server_live():
-    return jsonify({'message': 'server is live'})
+    return jsonify({"message": "server is live"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run()
--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,104 @@
+function updateData(google, textblobs) {
+  myChart.data.datasets[0].data = [google, textblobs];
+  myChart.update();
+}
+
 $('#google-form').submit(function(event) {
   event.preventDefault(); // prevent default form submit behavior
+  $("#google-prediction").html(`<p>sentiment: loading...</p>`)
+  $("#python-prediction").html(`<p>sentiment: loading...</p>`)
   var inputVal = $('#input-field').val();
   if (inputVal) {
-    fetch("https://dig345-p2-emotionai.vercel.app/predict/" + inputVal)
-    // fetch("http://localhost:5000/predict/" + inputVal)
+    fetch('https://dig345-p2-emotionai.vercel.app/predict', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: inputVal }),
+    })
     .then((response) => {
       // convert to json
       return response.json();
     })
     .then((data) => {
-      $("#google-prediction").html(`Prediction: ${data.google_sentiment}`)
-      $("#python-prediction").html(`Prediction: ${data.python_sentiment}`)
+      setTimeout(function() {
+        $("#google-prediction").html(`<p>sentiment: ${data.google_sentiment}</p>`)
+        $("#python-prediction").html(`<p>sentiment: ${data.python_sentiment}</p>`)
+        updateData(data.google_sentiment, data.python_sentiment)
+      }, Math.floor(Math.random() * 500) + 500);
     })
     .catch((err) => console.log(err));
   }
   else {
-    $("#google-prediction").html(`Prediction: no text`)
-    $("#python-prediction").html(`Prediction: no text`)
+    $("#google-prediction").html(`<p>sentiment: no text</p>`)
+    $("#python-prediction").html(`<p>sentiment: no text</p>`)
   }
 })
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Google Model', 'textBlobs'],
+    datasets: [{
+      label: 'Sentiment',
+      data: [0, 0],
+      backgroundColor:  function(context) {
+        const value = context.dataset.data[context.dataIndex];
+        const colorRed = { r: 255, g: 0, b: 0 };
+        const colorGrey = { r: 128, g: 128, b: 128 };
+        const colorGreen = { r: 0, g: 255, b: 0 };
+        let color;
+        if (value < 0) {
+          const valueNormalized = value / -1; // convert value to range of 0 to 1
+          color = {
+            r: colorGrey.r + (colorRed.r - colorGrey.r) * valueNormalized,
+            g: colorGrey.g + (colorRed.g - colorGrey.g) * valueNormalized,
+            b: colorGrey.b + (colorRed.b - colorGrey.b) * valueNormalized
+          };
+        } else if (value > 0) {
+          const valueNormalized = value / 1; // convert value to range of 0 to 1
+          color = {
+            r: colorGrey.r + (colorGreen.r - colorGrey.r) * valueNormalized,
+            g: colorGrey.g + (colorGreen.g - colorGrey.g) * valueNormalized,
+            b: colorGrey.b + (colorGreen.b - colorGrey.b) * valueNormalized
+          };
+        } else {
+          color = colorGrey;
+        }
+        return `rgba(${color.r}, ${color.g}, ${color.b}, 0.8)`;
+      },
+      borderColor: [
+        'rgba(0, 0, 0, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        min: -1,
+        max: 1,
+        ticks: {
+          stepSize: 0.2
+        },
+      },
+      y1: {
+        position: 'right',
+        min: -1,
+        max: 1,
+        ticks: {
+          stepSize: 0.2,
+        }
+      }
+    },
+    plugins: {
+      legend: {
+        display: false
+      }
+    },
+    response: true,
+    maintainAspectRatio: false
+  }
+});
+
